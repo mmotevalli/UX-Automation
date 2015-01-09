21d0b8cbad7252bbb7eb809b63b7e365c642f2a1
Motevalli <mmotevalli@gci-mmotevall-m.us.ad.gannett.com>
2015-01-09 09:39:14 -0500

    2nd commit

--- features/Domino_Order.feature
+++ features/Domino_Order.feature
@@ -1,6 +1,9 @@
+@dominoOrder
 Feature: Order Dominos Pizza Online
   We want to step through a simple scenario for training
  
 Scenario: Check the Dominos home page
   When I visit the Dominos home page
   Then I should see the Dominos home page
+  Then I will click on Menu
+  Then I will order Pizza
--- features/step_definitions/Domino_Order_steps.rb
+++ features/step_definitions/Domino_Order_steps.rb
@@ -1,7 +1,35 @@
 When(/^I visit the Dominos home page$/) do
-  visit('https://orders.dominos.com')
+  visit('https://order.dominos.com/en/')
 end
  
 Then(/^I should see the Dominos home page$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(page).to have_css('.js-siteMainNavigation')
+end
+
+Then(/^I will click on Menu$/) do
+    
+    first('.qa-Cl_Loc').click
+    find(".form__input--icon.Carryout").click
+    find("#Address_Type_Select").set("House")
+    first(".category card")
+    #find(first('.none--handheld')).click
+    find("#City").set("Herndon")
+    print "***************************"
+end
+When(/^I click carry out button$/) do
+    find(".form__input--icon.Carryout").click
+    end
+When(/^I enter details$/) do
+    find("#Address_Type_Select").set("House")
+    
+    find("#City").set("Herndon")
+    find("#Region").set("VA")
+    find("#Postal_Code").set("20171")
+    sleep 10
+    print "*************************"
+    
+    
+    first(".js-search-result").find(".btn--block").click
+  
+    find(".btn--large", :text => "CONTINUE").click
 end
